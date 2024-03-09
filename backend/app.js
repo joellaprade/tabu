f6b606e42508cb1320e921a5d284c2f3870d19cc
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-03-08 19:41:17 -0600

    s

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,12 @@ const port = 8000;
 // const baseDir = `para cPannel`;
 const baseDir = `C:\\Users\\dell user 2\\Documents\\GitHub\\tabu\\`;
 
-const dbURI = 'mongodb+srv://<user>:<pass>@nextcluster.gmdlrli.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(dbURI)
-    .then(result => {
-        app.listen(port)
-        console.log('listening' + String(date.getHours()+1) + ":" + String(date.getMinutes()))
-    })
+// const dbURI = 'mongodb+srv://<user>:<pass>@nextcluster.gmdlrli.mongodb.net/?retryWrites=true&w=majority'
+// mongoose.connect(dbURI)
+//     .then(result => {
+//         app.listen(port)
+//         console.log('listening' + String(date.getHours()+1) + ":" + String(date.getMinutes()))
+//     })
 
 app.use(express.static(baseDir));
 app.use(express.urlencoded({ extended: true }))
