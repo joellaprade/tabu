d0a4e13bc76d01c6e9a5291732896ec8440f919d
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-03-12 10:49:03 -0600

    finished header

--- components/head.js
+++ components/head.js
@@ -4,4 +4,13 @@ document.querySelector('head').innerHTML += `<link type="text/css" rel="styleshe
                                             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                                             <link href="" rel="stylesheet">
                                             <meta charset="UTF-8">
-                                            <meta name="viewport" content="width=device-width, initial-scale=1.0">`
+                                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                            
+<svg xmlns="http://www.w3.org/2000/svg">
+    <symbol id="hamburguer" viewBox="0 0 44 44">
+        <path d="M0 22C0 19.7909 1.79086 18 4 18H40C42.2091 18 44 19.7909 44 22C44 24.2091 42.2091 26 40 26H4C1.79086 26 0 24.2091 0 22Z" />
+        <path d="M0 4C0 1.79086 1.79086 0 4 0H40C42.2091 0 44 1.79086 44 4C44 6.20914 42.2091 8 40 8H4C1.79086 8 0 6.20914 0 4Z" />
+        <path d="M0 40C0 37.7909 1.79086 36 4 36H40C42.2091 36 44 37.7909 44 40C44 42.2091 42.2091 44 40 44H4C1.79086 44 0 42.2091 0 40Z" />
+    </symbol>
+</svg>
+`
---
+++ components/nav.js
@@ -0,0 +1,15 @@
+document.querySelector('nav').innerHTML += 
+`<div class="logo-container">
+    <img class="logo" src="../assets/logo.png" alt="">
+</div>
+<div class="links-container">
+    <a href="/">Home</a>
+    <a href="/menu">Menu</a>
+    <a href="/reserve">Reserve Venue</a>
+    <a class="link-button" href="#footer">Contact</a>
+    <svg class="hamburguer">
+        <use href="#hamburguer"></use>
+    </svg>
+</div>
+
+`
