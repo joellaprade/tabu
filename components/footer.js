619ab0ba9bec723873698b92dda8a6eba194dce0
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-03-16 12:37:05 -0600

    footer

---
+++ components/footer.js
@@ -0,0 +1,45 @@
+document.querySelector('footer').innerHTML += 
+`<div class="links">
+<div class="contact">
+    <h2>CONTACT</h2>
+    <div>
+        <svg class="phone">
+            <use href="/assets/footer/phone.svg#phone"></use>
+        </svg>
+        <a href="tel:22222222" target="_blank">22222222</a>
+    </div>
+    <div>
+        <svg class="whatsapp">
+            <use href="/assets/footer/whatsapp.svg#whatsapp"></use>
+        </svg>
+        <a href="tel:88888888" target="_blank">88888888</a>
+    </div>
+    <div>
+        <svg class="mail">
+            <use href="/assets/footer/mail.svg#mail"></use>
+        </svg>
+        <a href="mailto:administracion@tabutamarindo.com" target="_blank">administracion@tabutamarindo.com</a>
+    </div>
+</div>
+<div class="social">
+    <h2>SOCIAL</h2>
+    <a href="https://facebook.com" target="_blank">
+        <svg class="facebook">
+            <use href="/assets/footer/facebook.svg#facebook"></use>
+        </svg>
+    </a>
+    <a href="https://instagram.com" target="_blank">
+        <svg class="instagram">
+            <use href="/assets/footer/instagram.svg#instagram"></use>
+        </svg>
+    </a>
+    <a href="https://tripadvisor.com" target="_blank">
+        <svg class="trip-advisor">
+            <use href="/assets/footer/trip-advisor.svg#trip-advisor"></use>
+        </svg>
+    </a>
+</div>
+</div>
+<a href="https://maps.app.goo.gl/FXmKpF8Krh7hQggq8" target="_blank">
+<img src="/assets/footer/map.png" alt="">
+</a>`
