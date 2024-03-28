e574b256b8b30e871e1ce53d4e2f2c62d3a21bab
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-03-28 09:41:22 -0600

    s

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 8000;
-// const baseDir = `/home/jlaprade/tabu.jlaprade.com/`;
-const baseDir = `C:\\Users\\dell user 2\\Documents\\GitHub\\tabu\\`;
+const baseDir = `/home/jlaprade/tabu.jlaprade.com/`;
+// const baseDir = `C:\\Users\\dell user 2\\Documents\\GitHub\\tabu\\`;
 
-// const dbURI = 'mongodb+srv://<user>:<pass>@nextcluster.gmdlrli.mongodb.net/?retryWrites=true&w=majority'
-// mongoose.connect(dbURI)
-//     .then(result => {
-         app.listen(port)
-//         console.log('listening' + String(date.getHours()+1) + ":" + String(date.getMinutes()))
-//     })
-
+app.listen(port)
 app.use(express.static(baseDir));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
